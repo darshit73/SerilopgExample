798b7aec2216376cc1d50f5eb01d00b9bb7533aa
darshit73 <124660502+darshit73@users.noreply.github.com>
2023-03-01 10:47:47 +0530

    serilog

--- SerilogExample/Startup.cs
+++ SerilogExample/Startup.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace SerilogExample
 {
@@ -43,6 +40,7 @@ namespace SerilogExample
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseSerilogRequestLogging();
             app.UseRouting();
 
             app.UseAuthorization();
